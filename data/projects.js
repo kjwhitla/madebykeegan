64683f8791a413df391c378f41545a8933a74318
Keegan Whitla <kjwhitla@gmail.com>
2017-04-12 17:46:42 -0700

    feat: added projects.js within data/ This file is meant to house all necessary project information in the form of js objects

---
+++ data/projects.js
@@ -0,0 +1,36 @@
+window.projects = [
+
+    /** Web Projects **/
+
+	{
+        type: 'web',
+        title: 'first web project',
+        thumb: 'http://placehold.it/640x360',
+        width: '640',
+        height: '360',
+        externalUrl: 'http://madebykeegan.com',
+        meta: [
+
+        ]
+    },
+	{
+        type: 'web',
+        title: 'second web project',
+        thumb: 'http://placehold.it/640x360',
+        width: '640',
+        height: '360',
+        externalUrl: 'http://madebykeegan.com',
+        meta: [
+
+        ]
+    },
+    
+     /** Design projects */
+    {
+        type: 'design',
+        imageUrl: 'http://placehold.it/640x360',
+        externalUrl: 'http://madebykeegan.com',
+        title: 'Diplo - Chasing the Dragon'
+    }
+
+];
